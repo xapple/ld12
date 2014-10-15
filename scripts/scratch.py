f93d0761de579041c344d324e74e2df85907d008
Lucas Sinclair <lucas.sinclair@me.com>
2014-10-15 16:44:38 +0200

    Added scratch

---
+++ scripts/scratch.py
@@ -0,0 +1,5 @@
+from ld12.analysis import Analysis; out_dir = "/Users/sinclair/proj/b2013274/results"; a = Analysis(out_dir=out_dir)
+
+with open(a.p.user_dir + 'best_clusters.txt', 'w') as handle: handle.writelines('\n'.join([c.name for c in a.best_clusters]))
+
+with open(a.p.user_dir + 'fresh_clusters.txt', 'w') as handle: handle.writelines('\n'.join([c.name for c in a.fresh_clusters]))
