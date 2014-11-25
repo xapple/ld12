005cbd8ca953d592706cdd0a6fc0410940bff1f6
Lucas Sinclair <lucas.sinclair@me.com>
2014-11-25 14:14:03 +0100

    added counter

--- ld12/refseq.py
+++ ld12/refseq.py
@@ -8,6 +8,7 @@ from seqsearch.blast import BLASTdb
 from plumbing.autopaths import AutoPaths
 from plumbing.cache import property_cached
 from fasta.databases import refseq_bact_prot_nr, refseq_arch_prot_nr
+from fasta import FASTA
 
 # Third party modules #
 from shell_command import shell_output
@@ -43,19 +44,26 @@ class RefSeqProkPlusMarine(object):
         """Copy the files from the original refseq bacteria. Edit them
         to remove the things we don't want and return a list of the
         new files"""
+        # Message #
+        print "Filtering refseq database for SCGC genes..."
         # Two lists of files #
         orig_files     = list(refseq_bact_prot_nr.raw_files)
         modified_files = [FASTA(self.p.modified_dir + f.filename) for f in orig_files]
         # List of NCBI identifiers #
         identifiers = [g.info['Genome Name / Sample Name'][22:] for g in genomes.values() if g.fresh]
         # Filter function #
+        scgc_genes_found = 0
         def only_non_scgc(reads):
             for read in reads:
-                if any(i in read.description for i in identifiers): continue
+                if any(i in read.description for i in identifiers):
+                    scgc_genes_found =+ 1
+                    continue
                 else: yield read
         # Main loop #
         for orig, modif in zip(orig_files, modified_files): modif.write(only_non_scgc(orig))
         # Return #
+        print "Excluded %i SCGC genes" % scgc_genes_found
+        self.timer.print_elapsed()
         return modified_files
 
     @property_cached
